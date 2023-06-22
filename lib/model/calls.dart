c16ca1d674731861ce7fdcb478eafab904733b74
KingRaizel <88375935+KingRaizel@users.noreply.github.com>
2023-06-22 22:41:46 +0530

    updates dummy name in model

--- lib/model/calls.dart
+++ lib/model/calls.dart
@@ -10,27 +10,27 @@ class calls {
 
 List<calls> callData = [
   new calls(
-      name: "Hitesh Choudhary",
+      name: "John",
       time: "10 April, 6:03 pm",
       imageUrl:
           "https://images.pexels.com/photos/1149022/pexels-photo-1149022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new calls(
-      name: "Saksham Choudhary",
+      name: "Johnny",
       time: "10 April, 6:03 pm",
       imageUrl:
           "https://images.pexels.com/photos/1427288/pexels-photo-1427288.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new calls(
-      name: "Raushan Jha",
+      name: "Danielle",
       time: "10 April, 6:03 pm",
       imageUrl:
           "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new calls(
-      name: "Nitesh",
+      name: "Mark",
       time: "14 April, 6:08pm",
       imageUrl:
           "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new calls(
-      name: "Kaushal Jha",
+      name: "Sundar",
       time: "11 April, 7:03 pm",
       imageUrl:
           "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
--- lib/model/chat.dart
+++ lib/model/chat.dart
@@ -15,51 +15,51 @@ class chat {
 
 List<chat> messageData = [
   new chat(
-      name: "Hitesh Choudhary",
+      name: "John Doe",
       message: "Learn Code Online",
       time: "10:12",
       imageUrl:
           "https://images.pexels.com/photos/1149022/pexels-photo-1149022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Saksham",
+      name: "Elon Musk",
       message: "welcome to flutter course",
       time: "02:16",
       imageUrl:
           "https://images.pexels.com/photos/1427288/pexels-photo-1427288.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Raushan Jha",
+      name: "Mark",
       message:
           "I gave you the chance of aiding me willingly, but you have elected the way of pain.",
       time: "11:11",
       imageUrl:
           "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Nitesh",
+      name: "Ratan Tata",
       message: "Python ",
       time: "20:21",
       imageUrl:
           "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Kaushal Jha",
+      name: "Sundar Pichai",
       message: "The dark fire will not avail you! Flame of Udûn!",
       time: "1:12",
       imageUrl:
           "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Raushan Jha",
+      name: "Mukesh Ambani",
       message:
           "I gave you the chance of aiding me willingly, but you have elected the way of pain.",
       time: "11:11",
       imageUrl:
           "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Nitesh",
+      name: "Gautam Adani",
       message: "Python ",
       time: "20:21",
       imageUrl:
           "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
   new chat(
-      name: "Kaushal Jha",
+      name: "BIll Gates",
       message: "The dark fire will not avail you! Flame of Udûn!",
       time: "1:12",
       imageUrl:
